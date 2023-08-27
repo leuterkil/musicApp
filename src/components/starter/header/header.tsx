d3c14c10ae00de312d0178a7720d886f7ba40e54
Lefteris Charissopoulos <djfreedomofficial@gmail.com>
2023-08-27 20:03:42 +0300

    add logo

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -8,35 +8,10 @@ export default component$(() => {
       <div class={["container", styles.wrapper]}>
         <div class={styles.logo}>
           <a href="/" title="qwik">
-            <QwikLogo height={50} width={143} />
+            <img src="/logo2.png" height={50} width={143} />
           </a>
         </div>
-        <ul>
-          <li>
-            <a
-              href="https://qwik.builder.io/docs/components/overview/"
-              target="_blank"
-            >
-              Docs
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://qwik.builder.io/examples/introduction/hello-world/"
-              target="_blank"
-            >
-              Examples
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://qwik.builder.io/tutorial/welcome/overview/"
-              target="_blank"
-            >
-              Tutorials
-            </a>
-          </li>
-        </ul>
+       
       </div>
     </header>
   );
